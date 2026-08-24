9a99511c2ca4e42887f508de5cea18e622e828b8
Dirk P. Samson <23-01873@g.batstate-u.edu.ph>
2026-08-24 23:07:05 +0800

    Modified the init.py file based on the memory recollection feature

--- tools/__init__.py
+++ tools/__init__.py
@@ -3,12 +3,15 @@ from .calculator import calculate, calculator_schema
 from .file_reader import read_file, file_reader_schema
 from .file_writer import write_file, file_writer_schema
 from .web_search import search_web, web_search_schema
-tools = [time_tool_schema, calculator_schema, file_reader_schema, file_writer_schema, web_search_schema]
+from .memory import save_memory, recall_memory, save_memory_schema, recall_memory_schema
+tools = [time_tool_schema, calculator_schema, file_reader_schema, file_writer_schema, web_search_schema, save_memory_schema, recall_memory_schema]
 
 available_functions = {
     "get_current_time": get_current_time,
     "calculate": calculate,
     "read_file": read_file,
     "write_file": write_file,
-    "web_search": search_web
+    "web_search": search_web,
+    "save_memory": save_memory,
+    "recall_memory": recall_memory
 }
